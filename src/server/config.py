0931b2ea0ec5cc5c90b7f514ed26e4b24c808951
Alberto Serrano <ascalva@gmail.com>
2021-11-29 12:56:19 -0500

    Temporarily connect to local sqlite3 database, within server container

--- src/server/config.py
+++ src/server/config.py
@@ -5,3 +5,11 @@ class Config(object) :
     DEBUG      = True
     TESTING    = True
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+    # Simulation parameters.
+    COMMUNICATION_LIMIT = 2
+
+    BOARD_WIDTH  = 10
+    BOARD_HEIGHT = 10
