cf99498422222f788bbecc5bf12487d4106cd4d1
Alberto Serrano <ascalva@gmail.com>
2021-11-29 15:12:46 -0500

    Updated config params to connect to db

--- src/server/config.py
+++ src/server/config.py
@@ -6,7 +6,16 @@ class Config(object) :
     TESTING    = True
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+    RDS_ENGINE   = os.environ.get("RDS_ENGINE")
+    RDS_HOSTNAME = os.environ.get("RDS_HOSTNAME")
+    RDS_USERNAME = os.environ.get("RDS_USERNAME")
+    RDS_PASSWORD = os.environ.get("RDS_PASSWORD")
+    RDS_PORT     = os.environ.get("RDS_PORT")
+    RDS_DB_NAME  = os.environ.get("RDS_DB_NAME")
+
+    SQLALCHEMY_DATABASE_URI = \
+        f"{RDS_ENGINE}://{RDS_USERNAME}:{RDS_PASSWORD}@{RDS_HOSTNAME}:{RDS_PORT}/{RDS_DB_NAME}"
 
     # Simulation parameters.
     COMMUNICATION_LIMIT = 2
