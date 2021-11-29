900a427506ff1a7274ff4716a84fae1db097a1b0
Alberto Serrano <ascalva@gmail.com>
2021-11-29 17:01:18 -0500

    Updated get neighbors routes; still working on move endpoint

--- src/server/routes/actions.py
+++ src/server/routes/actions.py
@@ -1,7 +1,8 @@
-from flask                  import Blueprint, jsonify, request
-from flask_login            import current_user, login_user, logout_user, login_required
-from src.server             import app, db
-from src.server.models.user import User
+from flask             import Blueprint, jsonify, request
+from flask_login       import current_user, login_user, logout_user, login_required
+from src.server        import app, db
+from src.server.models import User, Obstacle
+from .environment      import get_empty_space
 
 
 actions = Blueprint("actions", __name__, url_prefix="/actions")
@@ -12,45 +13,42 @@ def client_login() :
     client_id = request.form.get("id")
     client_ip = request.remote_addr
 
-    # TODO: Create user (assign position?)
+    # Check if user exists, if not create and assign position.
     if (u := User.query.filter(User.id == client_id).first()) is None :
-        u = User(client_id, client_ip)
+        pos = get_empty_space()
+        u   = User(client_id, client_ip, *pos)
 
         db.session.add(u)
         db.session.commit()
-        print("Created user")
 
-        # TODO: Generate new position, validate nothing exists there.
-        pos_x, pos_y = 0, 0
-        message      = "Creating user"
+        message = "Created user."
 
     else:
-        print("user exists")
-        pos_x, pos_y = u.position
-        message      = "User already exists."
+        pos     = u.position
+        message = "User already exists."
 
-
-    # TODO: A
+    # Login user.
     login_user(u)
 
     return jsonify({
         "status"     : "ok",
         "message"    : message,
-        "position_x" : pos_x,
-        "position_y" : pos_y
+        "position"   : list(pos)
     })
 
 
-@actions.route("/getNeighbors", methods=["GET"], strict_slashes=False)
+@actions.route("/getNeighbors", methods=["GET", "POST"], strict_slashes=False)
 def getNeighbors() :
-    users = User.query.filter(User.id != current_user.id).all()
+    client_id = request.form.get("id")
+    users     = User.query.filter(User.id != client_id).all()
 
-    # TODO: Temporarily return all other users (broadcast).
-    return jsonify({c.id : c.client_ip for c in users})
+    # # TODO: Temporarily return all other users (broadcast).
+    return jsonify({c.id : c.ip_address for c in users})
 
 
 @actions.route("/move", methods=["POST"], strict_slashes=False)
 def move() :
+    client_id  = request.form.get("id")
     position_x = request.form.get("position_x")
     position_y = request.form.get("position_y")
 
